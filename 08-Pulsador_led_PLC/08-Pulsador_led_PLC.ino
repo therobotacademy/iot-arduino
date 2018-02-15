462f7da735e15cb89e80c1cb8aaa7a5939b6d63d
brjapon <brjapon@therobotacademy.com>
2018-02-15 16:37:04 +0100

    Added example '08-Pulsador_led_PLC.ino' for managing Arduino with plcLib. Version 'a08-Pulsador_led_PLC.ino' is done with standard code and has delayed responses and experiment bumps because of not using interrupts

---
+++ 08-Pulsador_led_PLC/08-Pulsador_led_PLC.ino
@@ -0,0 +1,23 @@
+#include <plcLib.h>
+
+/* Programmable Logic Controller Library for the Arduino and Compatibles
+
+   Bare Minimum - Single bit digital input and output
+
+   Connections:
+   Input - switch connected to input X1 (Arduino pin A1)
+   Output - LED connected to output Y1 (Arduino pin 5)
+
+   Software and Documentation:
+   https://github.com/wditch/plcLib
+
+*/
+
+void setup() {
+  setupPLC();  // Setup inputs and outputs
+}
+
+void loop() {
+  in(X1);      // Read Input 0
+  out(Y1);     // Send to Output 0
+}
---
+++ 08-Pulsador_led_PLC/a08-Pulsador_led_PLC/a08-Pulsador_led_PLC.ino
@@ -0,0 +1,30 @@
+int pushButton = 8;
+int pushButton_Analog = A1;
+int led = 5; // LED que responderá a la pulsación
+
+int buttonState = 0;  // Estado del pulsador
+int buttonAnalog = 0;  // Lectura analógica del pulsador
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(pushButton, INPUT);
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  buttonState = digitalRead(pushButton);
+  buttonAnalog = analogRead(pushButton_Analog);
+  Serial.print(buttonState);
+  delay(1);
+
+  if (buttonState == LOW) {   // Button NOT pressed
+    digitalWrite(led, LOW);
+    Serial.print("  Apago Led con valor analógico ");
+  }
+  else {
+    digitalWrite(led, HIGH);  // // Button pressed
+    Serial.print("\tEnciendo Led con valor analógico (voltios) ");
+  }
+  Serial.println((float)buttonAnalog/1023*5);
+  delay(200);
+}
