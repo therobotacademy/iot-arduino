bbb02a0551dbdfb53d82736c2b1eb53e54a04d9a
brjapon <brjapon@therobotacademy.com>
2018-02-26 11:29:50 +0100

    Theremin controlling a RGB led and solution for safe regulation of a crossing with traffic lights 's2.3_cruce_z_retardo_seguridad'

---
+++ 07-Theremin/07-Theremin_RGB/07-Theremin_RGB.ino
@@ -0,0 +1,46 @@
+/*
+Pseudo Thermin
+Outputs a color of the RGB depending on the range of frequency:
+  <1000 Hz        RED
+  1000-2000 Hz    GREEN
+  >2000 Hz        BLUE
+*/
+
+int speakerPin = 12;
+int photocellPin = 0;
+int redPin = 6;
+int greenPin = 9;
+int bluePin = 10;
+
+void setup()
+{
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);  
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int photocellReading = analogRead(photocellPin);
+  int pitch = 200 + photocellReading*4 ; // Factor 4 allows to cover the range 1000-3000 Hz for typical lighting in the training room (input 150-700 from LDR)
+  tone(speakerPin, pitch);
+  if(pitch <= 1000) setColor(255, 0, 0);  // red
+  else if (pitch > 1000 and pitch <= 2000) setColor(0, 255, 0);  // green
+  else if (pitch > 2000) setColor(0, 0, 255);  // blue
+
+  Serial.print("Analog reading = ");
+  Serial.print(photocellReading);     // the raw analog reading
+  Serial.print("\tPitch = ");
+  Serial.println(pitch);
+  //delay(10);
+  //noTone(speakerPin);
+  //delay(1000);
+}
+
+void setColor(int red, int green, int blue)
+{
+  analogWrite(redPin, red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue);  
+}
---
+++ 12- PROYECTO-CRUCE-SEMAFOROS-sketches/s2.3_cruce_z_retardo_seguridad/s2.3_cruce_z_retardo_seguridad.ino
@@ -0,0 +1,117 @@
+// Pines semáforo principal
+#define P_RED_PIN 8
+#define P_YELLOW_PIN 10
+#define P_GREEN_PIN 12
+
+// Pines semáforo secundario
+#define S_RED_PIN 2
+#define S_YELLOW_PIN 4
+#define S_GREEN_PIN 6
+
+// Estado del cruce
+int estado = 1;
+int estado_previo = 1;
+
+// Factor de escala de tiempos de estado
+float speed_ = 1;
+
+// Tiempos de estado (referencia)
+int E1 = 4000;
+int E2 = 1500;
+int E3 = 1000;
+int E4 = 2000;
+int E5 = 1500;
+
+void setup() {
+  Serial.begin(9600);
+  //Estado inicial
+  estado = 1;
+  // puertos a los que están conectados los LED - salidas
+  pinMode(P_RED_PIN, OUTPUT);
+  pinMode(P_YELLOW_PIN, OUTPUT);
+  pinMode(P_GREEN_PIN, OUTPUT);
+  pinMode(S_RED_PIN, OUTPUT);
+  pinMode(S_YELLOW_PIN, OUTPUT);
+  pinMode(S_GREEN_PIN, OUTPUT);
+}
+
+void loop() {
+  Serial.print("Estado "); Serial.print(estado); Serial.print(" ");
+  
+  // EJECUCIÓN DE ESTADO
+  switch (estado) {
+    case 1:
+      // ESTADO E1: Coches circulando en vía PRINCIPAL
+      // Vía PRINCIPAL
+      digitalWrite(P_RED_PIN, LOW); digitalWrite(P_YELLOW_PIN, LOW); digitalWrite(P_GREEN_PIN, HIGH);
+      // Vía SECUNDARIA
+      digitalWrite(S_RED_PIN, HIGH); digitalWrite(S_YELLOW_PIN, LOW); digitalWrite(S_GREEN_PIN, LOW);
+      // Volcado de info al puerto serie
+      Serial.print(E1*speed_); Serial.print(" ms"); Serial.print(" \t");
+      delay(E1*speed_);
+      break;
+    case 2:
+      // ESTADO E2: Transición a vía SECUNDARIA
+      // Vía PRINCIPAL
+      digitalWrite(P_RED_PIN, LOW); digitalWrite(P_YELLOW_PIN, HIGH); digitalWrite(P_GREEN_PIN, LOW);
+      // Vía SECUNDARIA
+      digitalWrite(S_RED_PIN, HIGH); digitalWrite(S_YELLOW_PIN, LOW); digitalWrite(S_GREEN_PIN, LOW);
+      // Volcado de info al puerto serie
+      Serial.print(E1*speed_); Serial.print(" ms"); Serial.print(estado); Serial.print(" \t");
+      delay(E2*speed_);
+      break;
+
+    case 3:
+      // ESTADO E3: Colchón de seguridad en la transición (2 semáforos en rojo)
+      // Vía PRINCIPAL
+      digitalWrite(P_RED_PIN, HIGH); digitalWrite(P_YELLOW_PIN, LOW); digitalWrite(P_GREEN_PIN, LOW);
+      // Vía SECUNDARIA
+      digitalWrite(S_RED_PIN, HIGH); digitalWrite(S_YELLOW_PIN, LOW); digitalWrite(S_GREEN_PIN, LOW);
+      // Volcado de info al puerto serie
+      Serial.print(E3*speed_); Serial.print(" ms"); Serial.print(estado); Serial.print(" \t");
+      delay(E3*speed_);
+      break;
+      
+    case 4:
+      // ESTADO E4: Coches circulando en vía SECUNDARIA
+      // Vía PRINCIPAL
+      digitalWrite(P_RED_PIN, HIGH); digitalWrite(P_YELLOW_PIN, LOW); digitalWrite(P_GREEN_PIN, LOW);
+      // Vía SECUNDARIA
+      digitalWrite(S_RED_PIN, LOW); digitalWrite(S_YELLOW_PIN, LOW); digitalWrite(S_GREEN_PIN, HIGH);
+      // Volcado de info al puerto serie
+      Serial.print(E4*speed_); Serial.print(" ms"); Serial.print(estado); Serial.print(" \t");
+      delay(E4*speed_);
+      break;
+    case 5:
+      // ESTADO E5: Transición a vía PRINCIPAL
+      // Vía PRINCIPAL
+      digitalWrite(P_RED_PIN, HIGH); digitalWrite(P_YELLOW_PIN, LOW); digitalWrite(P_GREEN_PIN, LOW);
+      // Vía SECUNDARIA
+      digitalWrite(S_RED_PIN, LOW); digitalWrite(S_YELLOW_PIN, HIGH); digitalWrite(S_GREEN_PIN, LOW);
+      // Volcado de info al puerto serie
+      Serial.print(E5*speed_); Serial.print(" ms"); Serial.print(estado); Serial.print(" \n");
+      delay(E5*speed_);
+      break;   
+    default:
+      // turn all the LEDs on:
+      for (int thisPin = 2; thisPin < 13; thisPin+=2) {
+          digitalWrite(thisPin, LOW);
+          Serial.print(thisPin); Serial.print(" ");
+          delay(200);
+        }
+        Serial.println(" ");
+  }
+  
+// DECISIÓN SOBRE EL SIGUIENTE ESTADO
+if (estado==3 && estado_previo==5) {
+  estado = 1;
+  estado_previo = 3;
+  }
+else {
+  estado_previo = estado;
+  estado++; // Transición al siguiente estado
+  if (estado > 5) {
+    estado = 3; // Transición a estado de retardo de seguridad
+    }
+  }
+}
