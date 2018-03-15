9375bcc2115381a19c51260e67783255fc91f182
brjapon <brjapon@therobotacademy.com>
2018-03-15 12:57:57 +0100

    '13a-delayed_light_off': Accounting for execution time to simulate 'real time'

--- 13a-delayed_light_off/13a-delayed_light_off.ino
+++ 13a-delayed_light_off/13a-delayed_light_off.ino
@@ -2,23 +2,36 @@
 #define BUTTON_PIN 8
 #define OFF 0
 #define ON 1
-int state = OFF;//Estado inicial con LED apagado
+
+// Control variables
+int state = OFF;  // Estado inicial con LED apagado
+int OffTime = 10000; 
+
+// Internal variables
 int timer = 0;
+long now, last;
+int loop_delay = 40; // Estimated iteration time delay to compute 'real time'
+                     // = estimated decrement to sinulate 'real time'
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_PIN, OUTPUT);
   pinMode(BUTTON_PIN, INPUT);
   Serial.begin (9600);
+  last = millis();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.print ("timer: ");
-  Serial.println (timer);
   LEDstep();
-  delay(10);
-  
+
+  // Time consumption evaluation
+  now = millis();
+  Serial.print("iteration time (ms)= "); // This line takes 30 ms!!!!!!
+  Serial.print(now-last);                // This line takes 3 ms
+  Serial.print ("\t timer: ");           // This line takes 14 ms
+  Serial.println (timer);                // This line takes 3 ms TOTALLING about 40 ms
+  last = now;  
 }
 
 void LEDstep() {
@@ -28,7 +41,7 @@ void LEDstep() {
     case OFF:
       if (button == HIGH) {
         state = ON;
-        timer = 1000;
+        timer = OffTime;
         analogWrite(LED_PIN,255);
       } break;
       
@@ -36,7 +49,7 @@ void LEDstep() {
       if (timer <= 0){
         state = OFF;
         analogWrite(LED_PIN,0);
-      } else timer--;
+      } else timer -= loop_delay;
   }
 }
 
---
+++ 13b-delayed_fading_light_off_timed/13b-delayed_fading_light_off_timed.ino
@@ -0,0 +1,70 @@
+//timed Light
+
+#define ON 1
+#define OFF 0
+#define Fading 2
+
+int buttonPin = 8;
+int ledPin = 11;
+int timer ;
+int timer2;
+int state = 0;
+unsigned int brillo;
+
+int t0; // Instant in which button is pressed
+int tON, tFADING;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  LEDstep();
+  delay(10);//ms
+
+}
+
+
+void LEDstep() {
+  int button;
+
+  Serial.print( state);
+  Serial.print("\t");  Serial.print(timer);
+  Serial.print("\t");  Serial.print(timer2);
+  Serial.print("\t"); Serial.println(brillo);
+
+  button = digitalRead(buttonPin);
+  switch (state) {
+    case OFF:
+      if (button == HIGH) {
+        state = ON;
+        t0 = millis();
+        timer = 2000; // ms, tiempo total de apagado
+        analogWrite(ledPin, 255);
+      } break;
+    case ON:
+      tON= millis();
+      if ((tON-t0) >= timer) {
+        state = Fading;
+        timer2 = 200;
+        break;
+      }
+      break;
+
+    case Fading:
+      tFADING= millis();
+      
+      if ((tFADING-t0) >= timer2) {
+        brillo = (255.0 * (timer2-(tFADING-t0)) / 200.0);
+        analogWrite(ledPin, brillo);
+      } else {
+        state = OFF;
+      }
+      break;
+  }
+}
+
+
